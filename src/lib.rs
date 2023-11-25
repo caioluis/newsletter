a89cf2796d09f5b12df3a1b3ad2ba1cb805a272e
Caio Gomes <ocaioluis@gmail.com>
2023-11-25 22:21:27 +0000

    deserialize email form

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Form, Router,
+};
+use serde::Deserialize;
 use std::net::SocketAddr;
 
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct UserInfo {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+async fn subscribe(Form(_user_info): Form<UserInfo>) -> StatusCode {
     StatusCode::OK
 }
 
@@ -16,5 +32,7 @@ pub async fn run() -> Result<(), std::io::Error> {
 }
 
 pub fn app() -> Router {
-    Router::new().route("/health-check", get(health_check))
+    Router::new()
+        .route("/health-check", get(health_check))
+        .route("/subscriptions", post(subscribe))
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -13,7 +13,24 @@ async fn health_check_works() {
                 .unwrap(),
         )
         .await
-        .unwrap();
+        .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let body = "name=caio%20gomes&email=me%40caio.codes";
+    let request = Request::builder()
+        .method("POST")
+        .uri("/subscriptions")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body.into())
+        .unwrap();
+
+    println!("request {:?}", request);
+    let response = app().oneshot(request).await.unwrap();
+    println!("response {:?}", response);
+
+    assert!(response.status().is_success())
 }
